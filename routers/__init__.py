102396c8d4831d5da56fd1ce1c9e9b251b721de9
lidongxun967 <1985409711@qq.com>
2025-09-03 22:19:51 +0800

    refactor(api): 移除未使用的工作流路由和相关代码

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,7 +1,6 @@
 import fastapi
-from . import wfs, forms, health
+from . import forms, health
 def include_routers(app: fastapi.FastAPI) -> fastapi.FastAPI:
-    app.include_router(wfs.router)
     app.include_router(forms.router)
     app.include_router(health.router)
     return app
